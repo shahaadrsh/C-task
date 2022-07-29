f53e2304443f67a8e760fae4b0d2edb0e1239740
shahaadrsh <shahaadrsh5@gmail.com>
2022-07-29 13:56:32 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include<string>
+using namespace std;
+//f(x)=x^2+2
+int add(int a,int b)
+ {
+    int c;
+    c = a + b;
+    return c;
+ }
+
+int main(){
+//cout<<"helllo world"<<endl;
+ // cout<<"next line";
+ // int a,b,c;
+ // short sa=9;
+ //cou
+ //_C89_NAMELESS 
+ //int marksInmaths=89;
+ //cout<<"The marks of a student in maths is  "<<marksInmaths;
+ 
+
+ //cout<<"The score is"<<score;
+//  int a, b;
+// cout<<"Enter first number"<<endl;
+//  cin>>a;
+
+// cout<<"Enter second number"<<endl;
+// cin>>b;
+
+// cout<<"a + b is"<<a + b<<endl;
+// cout<<"a - b is"<<a - b<<endl;
+// cout<<"a * b is"<<a * b<<endl;
+// cout<<"a / b is"<<(float)a / b<<endl;
+// int age;
+// cout<<"Enter your age "<<endl;
+// cin>>age;
+
+// switch (age)
+// {
+// case 12:
+//  cout<<"You are 12 year older ";
+//    break;
+//    case 18:
+//  cout<<"You are 18 year older ";
+//    break;
+
+// default:
+// cout<<"You are neither 12 nor 18 years old";
+//    break;
+// }
+
+
+// if (age>100 || age<1)
+// {
+// cout<<"INVALID AGE";
+// }
+// else if (age>=18){  
+//    cout<<"You can vote";}  
+// else
+// {
+//    cout<<"You cannot vote";
+// }
+// int index = 0;
+ //while(index<34)
+// {
+//     cout<<"We are in index number "<<index<<endl;
+//     index=index + 1;
+// }
+// do
+// {
+//     cout<<"We are in index number "<<index<<endl;
+//     index = index + 1;
+// }
+// while (index>3450    );
+// for (int i = 0; i < 34; i++)
+// { 
+// cout<<"The valuse of i is "<<i<<endl;
+//cout<<"My name is aadrsh shah";
+// }
+// int a, b;
+//  cout<<"Enter first number"<<endl;
+// cin>>a;
+
+//  cout<<"Enter second number"<<endl;
+//  cin>>b;
+//  cout<<"The function returend"<<add(a,b); 
+// int arr[3]=     {1,3,6};
+// //Array index=   0,1,2
+// cout<<arr[2];
+// for (int i = 0; i < 6; i++)
+// {
+//  cout<<"Marks of"<<i<<"th student is"<<marks[i]<<endl;
+// }
+// int arr2d[2][3]={
+//                   {1,2,3},
+//                   {4,5,6}};
+// for ( int i = 0; i < 2; i++)
+// {
+// for (int j = 0; j < 3; j++)
+// {
+//    cout<<"The value at "<<i<<","<<j<<"is"<<arr2d[i][j]<<endl;
+// }
+// }
+// int a=343;
+// float b=87.93;
+// cout<<(float) a/34<<endl;
+
+// cout<<(int)b;
+// string name ="harry";
+// cout<<"The name is "<<name<<endl;
+// cout<<"The length of  name is "<<name.length()<<endl;
+// cout<<"The name is "<<name.substr(1,150)<<endl;
+// cout<<"The name is "<<name.substr(2,3);
+// int a=34;
+// int* ptra;
+// ptra=&a;
+// cout<<"The value of a is"<<a<<endl;
+// cout<<"The value of a is"<<*ptra<<endl;
+// cout<<"The value of a is"<<&a<<endl;
+// cout<<"The value of a is"<<ptra<<endl;
+class Empolye
+   public:
+   string name;
+   int salary;
+}
+
+   return 0;
+}
+
